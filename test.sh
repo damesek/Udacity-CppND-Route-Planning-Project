ac5cb3627724705afba0419a315de4ac9588b4f8
damesek <sz@baader.hu>
2019-05-29 15:25:07 +0200

    Ubuntu18.04

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# full reset and create directory
+rm -rf ./build && mkdir build && cd build
+
+# choose the right test
+#cmake ..
+cmake .. "-DTESTING=RouteModel" 
+#cmake .. "-DTESTING=RMNodeClass" 
+#cmake .. "-DTESTING=RMSNodes" 
+#cmake .. "-DTESTING=NodeDist" 
+#cmake .. "-DTESTING=NodeToRoad" 
+#cmake .. "-DTESTING=FindNeighbors" 
+#cmake .. "-DTESTING=FindClosest" 
+#cmake .. "-DTESTING=AStarStub" 
+#cmake .. "-DTESTING=AStarSearch"
+
+
+make 
+../bin/test
