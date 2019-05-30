d3bc813c884f95334e2b8b77fc353d0845c20137
damesek <sz@baader.hu>
2019-05-30 13:04:40 +0200

    with user input Done

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,24 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below.
 
+    float start_x, start_y, end_x, end_y;
+
+     std::cout << "*********** INPUT COORDINATES **************" << std::endl;
+     std::cout << "Enter X starting position between 0 and 100:" << std::endl;
+     std::cin >> start_x; 
+     std::cout << "Enter Y starting position between 0 and 100:" << std::endl;
+     std::cin >> start_y; 
+     std::cout << "Enter X ending position between 0 and 100:" << std::endl;
+     std::cin >> end_x;
+     std::cout << "Enter Y ending position between 0 and 100:" << std::endl;
+     std::cin >> end_y;
+
     // Build Model.
     RouteModel model{osm_data};
 
     // Perform search and render results.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    //RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
     std::cout << "Distance:  " << route_planner.GetDistance() << " meters. \n";
     Render render{model};
